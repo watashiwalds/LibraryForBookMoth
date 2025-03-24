b66c93abea5d0d2f3e959af8c93934fbef3b69de
watashiwalds <143416825+watashiwalds@users.noreply.github.com>
2025-03-24 18:15:18 +0700

    accept new click listener

--- app/src/main/java/com/lsdapps/uni/bookmoth_library/library/ui/viewholder/ChapterItemRecyclerViewHolder.java
+++ app/src/main/java/com/lsdapps/uni/bookmoth_library/library/ui/viewholder/ChapterItemRecyclerViewHolder.java
@@ -7,16 +7,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.lsdapps.uni.bookmoth_library.R;
+import com.lsdapps.uni.bookmoth_library.library.ui.adapter.OnItemClickListener;
 
 public class ChapterItemRecyclerViewHolder extends RecyclerView.ViewHolder {
     public TextView index;
     public TextView pdate;
     public TextView title;
 
-    public ChapterItemRecyclerViewHolder(@NonNull View itemView) {
+    public ChapterItemRecyclerViewHolder(@NonNull View itemView, OnItemClickListener listener) {
         super(itemView);
         index = itemView.findViewById(R.id.chapter_index);
         pdate = itemView.findViewById(R.id.chapter_pdate);
         title = itemView.findViewById(R.id.chapter_title);
+
+        itemView.setOnClickListener(v -> {
+            if (listener != null) {
+                int pos = getAdapterPosition();
+                if (pos != RecyclerView.NO_POSITION) {
+                    listener.onItemClick(pos);
+                }
+            }
+        });
     }
 }
