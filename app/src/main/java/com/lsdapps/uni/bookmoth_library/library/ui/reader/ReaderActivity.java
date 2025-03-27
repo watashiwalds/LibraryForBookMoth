88eb2d528dc59d6adf441a03f855f136e776a3fd
watashiwalds <143416825+watashiwalds@users.noreply.github.com>
2025-03-27 14:32:46 +0700

    basic visual done, go on with data display

--- app/src/main/java/com/lsdapps/uni/bookmoth_library/library/ui/reader/ReaderActivity.java
+++ app/src/main/java/com/lsdapps/uni/bookmoth_library/library/ui/reader/ReaderActivity.java
@@ -1,10 +1,12 @@
 package com.lsdapps.uni.bookmoth_library.library.ui.reader;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,33 +49,74 @@ public class ReaderActivity extends AppCompatActivity {
         contentView = findViewById(R.id.rdr_tv_content);
     }
 
-    private Boolean headerBarVisible = true;
+    private Boolean barVisible = true;
+    private void setNavbarVisibility(Boolean bool) {
+        if (bool && !barVisible) {
+            barVisible = true;
+            UniversalAnimate.animateWallHiding(headerBar, UniversalAnimate.PLACEMENT_TOP, false);
+            UniversalAnimate.animateWallHiding(bottomBar, UniversalAnimate.PLACEMENT_BOTTOM, false);
+        } else if (!bool && barVisible) {
+            barVisible = false;
+            UniversalAnimate.animateWallHiding(headerBar, UniversalAnimate.PLACEMENT_TOP, true);
+            UniversalAnimate.animateWallHiding(bottomBar, UniversalAnimate.PLACEMENT_BOTTOM, true);
+        }
+    }
+
     private void initGraphical() {
         headerBar.addView(getLayoutInflater().inflate(R.layout.toolbar_rdr_header, headerBar, false));
         bottomBar.addView(getLayoutInflater().inflate(R.layout.toolbar_rdr_bottom, bottomBar, false));
-        nestedContainer.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
-            if (scrollY > oldScrollY) {
-                // Hide Toolbar
-                if (headerBarVisible) {
-                    headerBarVisible = false;
-                    UniversalAnimate.animateWallHiding(headerBar, UniversalAnimate.PLACEMENT_TOP, true);
+        nestedContainer.post(() -> {
+            nestedContainer.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+                if (scrollY == 0 || scrollY >= (nestedContainer.getChildAt(0).getHeight() - nestedContainer.getHeight() - 1)) {
+                    setNavbarVisibility(true);
+                    return;
                 }
-            } else if (scrollY < oldScrollY) {
-                // Show Toolbar
-                if (!headerBarVisible) {
-                    headerBarVisible = true;
-                    UniversalAnimate.animateWallHiding(headerBar, UniversalAnimate.PLACEMENT_TOP, false);
-                }
-            }
+                if (scrollY > oldScrollY) setNavbarVisibility(false);
+            });
         });
         contentView.setOnClickListener(view -> {
-            Toast.makeText(this, "Clicked the NestedView", Toast.LENGTH_SHORT).show();
-            UniversalAnimate.animateWallHiding(bottomBar, UniversalAnimate.PLACEMENT_BOTTOM, bottomBar.getTranslationX() <= 0);
-            UniversalAnimate.animateWallHiding(headerBar, UniversalAnimate.PLACEMENT_TOP, headerBar.getTranslationX() >= 0);
+            setNavbarVisibility(!barVisible);
         });
     }
 
     private void initFunctions() {
+        contentView.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                menu.clear();
+                menu.add(0, 1, 0, R.string.contentselection_discuss).setOnMenuItemClickListener(item -> {
+                    Toast.makeText(contentView.getContext(), "in-dev, post related", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                });
+                menu.add(0, 2, 1, R.string.contentselection_search).setOnMenuItemClickListener(item -> {
+                    Toast.makeText(contentView.getContext(), "in-dev, post/store related", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                });
+                menu.add(0, 3, 2, R.string.contentselection_report).setOnMenuItemClickListener(item -> {
+                    Toast.makeText(contentView.getContext(), "in-dev, community related", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                });
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        }); //custom action for selection
         headerBar.findViewById(R.id.imgbtn_back).setOnClickListener(v -> {
             finish();
         });
