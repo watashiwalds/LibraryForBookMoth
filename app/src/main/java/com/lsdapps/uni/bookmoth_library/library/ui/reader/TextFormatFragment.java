97612a9a7df2a1d0004020be96d353b1e9bc5381
watashiwalds <143416825+watashiwalds@users.noreply.github.com>
2025-03-28 15:06:34 +0700

    display spinner for font family change

--- app/src/main/java/com/lsdapps/uni/bookmoth_library/library/ui/reader/TextFormatFragment.java
+++ app/src/main/java/com/lsdapps/uni/bookmoth_library/library/ui/reader/TextFormatFragment.java
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.lsdapps.uni.bookmoth_library.R;
@@ -19,6 +21,9 @@ public class TextFormatFragment extends Fragment {
 
     private SeekBar textSize;
     private ArrayList<TextView> textDemos = new ArrayList<>();;
+    private Spinner fontFamily;
+    private ArrayList<String> availableFonts = new ArrayList<>();
+    private ArrayAdapter<String> fontListAdapter;
 
     private float ori_textSize;
     private float var_textSize;
@@ -40,12 +45,20 @@ public class TextFormatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_reader_textformat, container, false);
+
         ori_textSize = contentView.getTextSize() / requireContext().getResources().getDisplayMetrics().scaledDensity;
         textSize = v.findViewById(R.id.rdr_stg_textsize);
         textDemos.add(v.findViewById(R.id.textdemo_regular));
         textDemos.add(v.findViewById(R.id.textdemo_bold));
         textDemos.add(v.findViewById(R.id.textdemo_italic));
         textDemos.add(v.findViewById(R.id.textdemo_bolditalic));
+
+        fontFamily = v.findViewById(R.id.rdr_sp_fontfamily);
+        availableFonts.add("Alegreya");
+        availableFonts.add("Bitter");
+        availableFonts.add("Bookely");
+        availableFonts.add("Literata");
+
         initialize();
         return v;
     }
@@ -64,5 +77,9 @@ public class TextFormatFragment extends Fragment {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
+
+        fontListAdapter = new ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, availableFonts);
+        fontFamily.setAdapter(fontListAdapter);
+        fontListAdapter.notifyDataSetChanged();
     }
 }
