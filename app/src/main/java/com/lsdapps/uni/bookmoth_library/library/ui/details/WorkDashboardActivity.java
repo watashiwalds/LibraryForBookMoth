a990f5646fd150a154c4a20f05221e2fd256124c
watashiwalds <watashiwalds@users.noreply.github.com>
2025-04-01 23:13:38 +0700

    wip workdashboard

---
+++ app/src/main/java/com/lsdapps/uni/bookmoth_library/library/ui/details/WorkDashboardActivity.java
@@ -0,0 +1,26 @@
+package com.lsdapps.uni.bookmoth_library.library.ui.details;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.lsdapps.uni.bookmoth_library.R;
+
+public class WorkDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_work_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
